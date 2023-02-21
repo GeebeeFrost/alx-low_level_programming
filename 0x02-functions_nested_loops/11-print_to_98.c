7a064a07fb83989b01303d768ce85345dbf84054
GeebeeFrost <omgbolahan1@gmail.com>
2023-02-21 16:52:16 +0000

    Add function t o print natural numbers ending at 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,26 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n > 98)
 	{
-		printf("%d", i);
-		if (i == 98)
-			return;
-		printf(", ");
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i == 98)
+			{
+				printf("\n");
+				return;
+			printf(", ");
+			}
+		}
 	}
-	_putchar('\n');
+	else
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i == 98)
+				printf("\n");
+				return;
+			printf(", ");
+		}
 }
