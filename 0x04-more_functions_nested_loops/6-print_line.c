0f05a93f71856201fbfc36b28c2ab75cc2c1bbdf
GeebeeFrost <omgbolahan1@gmail.com>
2023-02-23 17:33:18 +0000

    Add function to print underscores

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,14 +6,9 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
 	int i;
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 		_putchar('_');
 	_putchar('\n');
 }
