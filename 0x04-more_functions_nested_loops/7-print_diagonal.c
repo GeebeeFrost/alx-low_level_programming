4b0baa405ee19fec677cd2a00884e6fcfc9afe07
GeebeeFrost <omgbolahan1@gmail.com>
2023-02-23 17:46:07 +0000

    Add function to print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws diagonal line
+ * @n: Number of '\' to draw
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int spaces;
+
+	for (i = 0; i < n; i++)
+	{
+		for (spaces = 0; spaces < i; spaces++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
