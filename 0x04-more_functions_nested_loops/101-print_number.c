1f7ae73c267267901e9a06b2f9668c636d5ddeb4
GeebeeFrost <omgbolahan1@gmail.com>
2023-02-24 03:57:04 -0800

    Add function to print integer using _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,18 +6,18 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int n1 = 0;
 
 	if (n < 0)
 	{
-		_putchar('-');
 		n1 = -n;
+		_putchar('-');
 	}
 	else
 	{
 		n1 = n;
 	}
-	if (n1 >= 10)
+	if (n1 / 10)
 		print_number(n1 / 10);
 	_putchar((n % 10) + '0');
 }
