42197fa5f012a2935ce656bbc6e32258a503d745
GeebeeFrost <omgbolahan1@gmail.com>
2023-02-27 17:54:25 +0000

    Add function to print last half of string

---
+++ 0x05-pointers_arrays_strings/7-puts-half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints second half of string followed by new line.
+ * @str: String to be printed half of
+ */
+void puts_half(char *str)
+{
+	int length, start;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{}
+
+	if (length % 2 == 0)
+		start = length / 2;
+	else
+		start = ((length - 1) / 2) + 1;
+
+	for (i = start; i <= length - 1; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
