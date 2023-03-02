daf2897762452f935706b1942529934774d9e5a4
GeebeeFrost <omgbolahan1@gmail.com>
2023-03-02 14:04:05 +0000

    Add function to compare two strinf

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] == s2[i] && s1[i] != '\0')
 		i++;
-	if (s1 != s2)
-		res = *s1 - *s2;
+	if (s1[i] != s2[i])
+		res = s1[i] - s2[i];
 	return (res);
 }
