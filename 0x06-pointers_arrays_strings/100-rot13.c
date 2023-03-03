6cc1e9024111de3f9e01c57528cbd691d70f23a7
GeebeeFrost <omgbolahan1@gmail.com>
2023-03-03 16:17:24 +0000

    Add function to encode string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -17,9 +17,9 @@ char *rot13(char *str)
 		{
 			if ((str[i] >= 'a' && str[i] <= 'm') ||
 					(str[i] >= 'A' && str[i] <= 'M'))
-				str[i] += 13;
+				str[i] = str[i] + 13;
 			else
-				str[i] -= 13;
+				str[i] = str[i] - 13;
 			i++;
 		}
 		i++;
