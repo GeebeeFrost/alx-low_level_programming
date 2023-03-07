1ac9e3519479b8f0dfde242e67cdae986059acc8
GeebeeFrost <omgbolahan1@gmail.com>
2023-03-07 12:53:49 +0100

    Correct strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,14 +6,11 @@
  *
  * Return: Length of string
  */
-int length = 0;
 int _strlen_recursion(char *s)
 {
-
-	if (*s != '\0')
+	if (!*s)
 	{
-		length++;
-		_strlen_recursion(s + 1);
+		return (0);
 	}
-	return (length);
+	return (1 + _strlen_recursion(++s));
 }
