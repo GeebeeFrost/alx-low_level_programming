69f58cd6837b8a44ea28597b3721974799306fdc
GeebeeFrost <omgbolahan1@gmail.com>
2023-03-14 11:41:39 +0000

    Add function to concatenate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strilen - Returns the length of a string
+ * @s: String
+ *
+ * Return: Length of string;
+ */
+int strilen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (*(s + length) != '\0')
+		length++;
+	return (length);
+}
+
+/**
+ * str_concat - Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Pointer to new concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *result;
+	unsigned int i, j, length;
+
+	if (s1 == NULL)
+		length = 0 + strilen(s2);
+	else if (s2 == NULL)
+		length = strilen(s1) + 0;
+	else
+		length = strilen(s1) + strilen(s2);
+	result = malloc(sizeof(char) * (length + 1));
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (*(s1 + i) != '\0')
+	{
+		*(result + i) = *(s1 + i);
+		i++;
+	}
+	j = 0;
+	while (*(s2 + j) != '\0')
+	{
+		*(result + i) = *(s2 + j);
+		i++;
+		j++;
+	}
+	return (result);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,5 +4,7 @@
 int _putchar(char);
 char *create_array(unsigned int, char);
 char *_strdup(char *);
+int strilen(char *);
+char *str_concat(char *, char *);
 
 #endif
