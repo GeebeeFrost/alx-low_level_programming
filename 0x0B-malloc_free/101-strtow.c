c0669f451fa18a992fceaa576df2cf70569eea9f
GeebeeFrost <omgbolahan1@gmail.com>
2023-03-15 17:34:11 +0100

    Create 101-strtow

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - Splits string into words
+ * @str: String to split
+ *
+ * Return: Pointer to array of strings
+ */
+char **strtow(char *str)
+{
+}
