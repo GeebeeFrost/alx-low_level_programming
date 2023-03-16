b62b07678b1f3a248c3af99e6a3346cba46c9e0b
GeebeeFrost <omgbolahan1@gmail.com>
2023-03-16 15:12:55 +0000

    Add function to mimic calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array
+ * @nmemb: Number of elements
+ * @size: Size of each element
+ *
+ * Return: Pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *result;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	result = malloc(size * nmemb);
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		result[i] = 0;
+	return (result);
+}
