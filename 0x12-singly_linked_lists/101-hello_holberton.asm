e3759ccf1561b5897656a7dfd8307d8dea5585dd
GeebeeFrost <omgbolahan1@gmail.com>
2023-03-30 15:04:48 +0000

    Correct 101-hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,15 +3,11 @@ msg:	db "Hello, Holberton", 0
 fmt:	db "%s", 10, 0
 
 	SECTION .text
-	extern printf
 	global main
+	extern printf
 main:
-_start:
-	mov esi, msg	;
-	mov edi, fmt	;
-	mov eax, 0	;
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
 	call printf
-
-	mov ebx, 0	;
-	mov eax, 1	;
-	int 0x80	;
+	ret
