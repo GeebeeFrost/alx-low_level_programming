b8851484724508c5eaceb8cd8745c9506e9937b6
GeebeeFrost <omgbolahan1@gmail.com>
2023-04-04 11:06:54 +0000

    Correct 5-freelistint2

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes the head node of a linked list of integers
+ * and returns the head node's data
+ * @head: Pointer to pointer to head node
+ *
+ * Return: Head node's data or 0 if list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	int result = 0;
+	listint_t *temp = *head;
+
+	while (temp != NULL)
+	{
+		
+	}
+	return (result);
+}
