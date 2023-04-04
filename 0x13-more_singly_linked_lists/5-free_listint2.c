a1ca8b74af7365010738f2f3f65c0ac08d35c666
GeebeeFrost <omgbolahan1@gmail.com>
2023-04-04 14:15:56 +0000

    Correct 5-freelistint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,17 +3,19 @@
 /**
  * free_listint2 - Frees a linked list of integers
  * and sets the head to NULL
- * @head: Pointer to head node
+ * @head: Address of pointer to head node
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-	head = NULL;
+	*head = NULL;
 }
