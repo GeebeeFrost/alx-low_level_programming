3fb479a465643321036ec58e88534709ad0d8f7e
GeebeeFrost <omgbolahan1@gmail.com>
2023-04-04 13:14:25 +0000

    Correct 9-insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,6 +33,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (i = 0; i < (idx - 1); i++)
 	{
+		if (temp == NULL)
+			return (NULL);
 		temp = temp->next;
 	}
 	new->next = temp->next;
