083ea8227a2c12eb792f573f4576e1f2f076ade6
GeebeeFrost <omgbolahan1@gmail.com>
2023-05-18 17:58:44 +0000

    Correct 7-insert_dnodeint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new = add_dnodeint_end(h, n);
 			return (new);
 		}
-		else if (i = (idx - 1))
+		else if (i == (idx - 1))
 		{
 			new->next = temp->next;
 			new->prev = temp;
