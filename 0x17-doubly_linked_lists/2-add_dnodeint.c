5bade85d101bff339680305fa635e014bd2dc49d
GeebeeFrost <omgbolahan1@gmail.com>
2023-05-18 14:56:41 +0000

    Correct 2-add_dnodeint

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,12 +18,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	new->n = n;
 	new->prev = NULL;
-	new->next = *head;
-	*head = new;
+	new->next = NULL;
 	if (!*head)
 	{
+		*head = new;
 		return (new);
 	}
+	new->next = *head;
 	(*head)->prev = new;
+	*head = new;
 	return (new);
 }
