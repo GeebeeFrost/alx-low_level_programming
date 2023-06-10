849f78883ab6965099c216404d0363d44d4adfdc
GeebeeFrost <omgbolahan1@gmail.com>
2023-06-10 01:20:16 +0000

    Correct 3-hash_table_set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new, *check, *temp;
 	unsigned long int index;
 
-	if (!ht)
+	if (!ht || *key == '\n' || *value == '\n')
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	check = ht->array[index];
